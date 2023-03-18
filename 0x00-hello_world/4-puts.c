c8973cad825d47b1ec23cb34c7eb20251a15ea6c
imadben10 <off.imad@gmail.com>
2023-03-18 12:21:08 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,7 @@
-#!/bin/bash
-echo "Programming is like building a multilingual puzzle" | puts
-exit 0
+#include <stdio.h>
+
+int main(void)
+{
+  puts("Programming is like building a multilingual puzzle\n");
+  return 0;
+}
