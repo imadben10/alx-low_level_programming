f4a35c799be2534b6e2f47d60bd007b99cfa727c
imadben10 <off.imad@gmail.com>
2023-03-18 13:38:06 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,12 @@
+#include <unistd.h>
+
 /**
 * main - Entry point
 *
-* Return: Always 1 (Error)
+* Return: Always 1 (error code)
 */
 int main(void)
 {
-/* your code here */
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
