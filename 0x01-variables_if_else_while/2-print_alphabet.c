48aabe7aa2e4877cb08dbd89c79866d465edb97d
imadben10 <off.imad@gmail.com>
2023-03-20 19:13:58 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
 int main(void)
 {
-char letter = 'a';
+char letter = 'a';  // start with 'a'
+// loop through each letter of the alphabet
 while (letter <= 'z')
 {
-putchar(letter);
-letter++;
+putchar(letter);  // print the current letter
+letter++;         // move on to the next letter
 }
-putchar('\n');
+putchar('\n');       // print a newline character
 return 0;
 }
