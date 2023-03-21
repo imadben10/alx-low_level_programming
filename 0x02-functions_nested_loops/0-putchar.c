1ae331c76cb090655354f4491e64507d4ff0c738
imadben10 <off.imad@gmail.com>
2023-03-21 14:52:02 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include "main.h"
-/*
+
+/**
 * main - Entry point
-* Description: the putchar function to output the string "_putchar" 
+*
+* Description: Use the putchar function to output the string "_putchar"
+* Return: Always 0 (Success)
 */
 int main(void)
 {
-/* Output each character in the string "_putchar" using putchar()*/
 putchar('_');
 putchar('p');
 putchar('u');
@@ -15,8 +17,6 @@ putchar('c');
 putchar('h');
 putchar('a');
 putchar('r');
-/* Output a newline character to the console*/
 putchar('\n');
-/* Exit the program with a return value of 0 to indicate successful execution*/
 return (0);
 }
