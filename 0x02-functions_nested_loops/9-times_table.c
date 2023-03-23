646478caa9f1ac8754b55964e7700016023b617b
imadben10 <off.imad@gmail.com>
2023-03-22 17:53:22 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* times_table - Prints the 9 times table
+* Return: void
+*/
+void times_table(void)
+{
+int row, col, res;
+for (row = 0; row <= 9; row++)
+{
+_putchar('0');
+for (col = 1; col <= 9; col++)
+{
+_putchar(',');
+_putchar(' ');
+res = row * col;
+if (res <= 9)
+_putchar(' ');
+else
+_putchar((res / 10) + '0');
+_putchar((res % 10) + '0');
+}
+_putchar('\n');
+}
+}
