63d22b20d66d335d7c7742ca8bcf187dc018e8fa
imadben10 <off.imad@gmail.com>
2023-03-22 18:59:35 -0700

    101-natural

---
+++ 0x02-functions_nested_loops/101-main.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <stdio.h>
+int main(void)
+{
+int limit = 1024;
+int result = sum_multiples(limit);
+printf("%d\n", result);
+return 0;
+}
--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,11 +1,19 @@
-#include <stdio.h>
-int main(void) {
+#include "main.h"
+/**
+* sum_multiples - compute the sum of all multiples of 3 or 5 below a limit
+* @limit: the upper limit
+*
+* Return: the sum of all multiples of 3 or 5 below the limit
+*/
+int sum_multiples(int limit)
+{
 int sum = 0;
-for (int i = 1; i < 1024; i++) {
-if (i % 3 == 0 || i % 5 == 0) {
+for (int i = 1; i < limit; i++)
+{
+if (i % 3 == 0 || i % 5 == 0)
+{
 sum += i;
 }
 }
-printf("%d\n", sum);
-return 0;
+return sum;
 }
