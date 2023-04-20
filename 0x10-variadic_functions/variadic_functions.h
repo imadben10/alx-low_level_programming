cd20af048474e173933f3fa7ca34a734837393c2
imadben10 <off.imad@gmail.com>
2023-04-20 16:43:14 -0700

    0x10-variadic_functions

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,17 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+#include <stdarg.h>
+/**
+* struct print - struct print
+* @type: type to print
+* @func: function to print
+*/
+typedef struct print
+{
+char *type;
+void (*func)(va_list arg);
+} print_t;
+int sum_them_all(const unsigned int n, ...);
+void print_all(const char * const format, ...);
+#endif
