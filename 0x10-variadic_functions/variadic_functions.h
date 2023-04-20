a6f1eb019790bd37fd2469527e1b9a189db04a46
imadben10 <off.imad@gmail.com>
2023-04-20 16:48:35 -0700

    2-print_strings.c

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -15,5 +15,6 @@ void (*func)(va_list arg);
 int sum_them_all(const unsigned int n, ...);
 void print_all(const char * const format, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
 
 #endif
