ddf1043baf72a7db062453d4eec568f193d22496
imadben10 <off.imad@gmail.com>
2023-04-20 16:22:43 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,17 +1,28 @@
+#include <stddef.h>
 #include "function_pointers.h"
 /**
-* array_iterator - executes a function given as a parameter on each element of an array.
-* @array: the array to iterate through
-* @size: the size of the array
-* @action: a pointer to the function to use
-* Return: Nothing.
+* print_name - Prints a name.
+* @name: A pointer to a string that contains a name.
+* @f: A pointer to a function that prints a name.
+*
+* Return: void.
+*/
+void print_name(char *name, void (*f)(char *))
+{
+if (name && f)
+f(name);
+}
+/**
+* array_iterator - Executes a function on each element of an array.
+* @array: A pointer to an array of integers.
+* @size: The size of the array.
+* @action: A pointer to a function that takes an integer as an argument.
+* Return: void.
 */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 size_t i;
 if (array && action)
-{
 for (i = 0; i < size; i++)
 action(array[i]);
-}
 }
