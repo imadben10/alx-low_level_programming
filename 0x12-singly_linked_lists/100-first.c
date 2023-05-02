e723f42c7d1db75765bd35e98cfe2db9f09ad311
imadben10 <off.imad@gmail.com>
2023-05-02 08:36:47 -0700

    updating

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,16 @@
+#ifndef _FIRST_H_
+#define _FIRST_H_
+
 #include <stdio.h>
 
-void __attribute__ ((constructor)) before_main()
+void __attribute__ ((constructor)) print_msg();
+
+#endif /* _FIRST_H_ */
+
+/**
+ * print_msg - prints a message before the main function is executed
+ */
+void print_msg(void)
 {
     printf("You're beat! and yet, you must allow,\n"
            "I bore my house upon my back!\n");
