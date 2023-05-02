13e509e94b0de1852e602c64e64bee5f0467375d
imadben10 <off.imad@gmail.com>
2023-05-02 08:29:24 -0700

    updating

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,52 +1,40 @@
+#include "lists.h"
 #include <stdlib.h>
 #include <string.h>
-#include "lists.h"
 
 /**
- * add_node_end - adds a new node at the end of a list_t list
+ * add_node_end - adds a new node at the end of a list_t list.
  * @head: pointer to the head of the list
  * @str: string to be added to the new node
  *
- * Return: address of the new element or NULL if it failed
+ * Return: address of the new element or NULL if it fails.
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *new_node, *last_node;
-    char *dup_str;
-    size_t len;
+	list_t *new_node, *temp_node;
+	size_t length = 0;
 
-    if (str == NULL)
-        return (NULL);
+	new_node = malloc(sizeof(list_t));
+	if (!new_node)
+		return (NULL);
 
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-        return (NULL);
+	new_node->str = strdup(str);
+	if (new_node->str)
+	{
+		while (new_node->str[length])
+			length++;
+	}
+	new_node->len = length;
+	new_node->next = NULL;
 
-    dup_str = strdup(str);
-    if (dup_str == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
-
-    len = 0;
-    while (str[len])
-        len++;
-
-    new_node->str = dup_str;
-    new_node->len = len;
-    new_node->next = NULL;
-
-    if (*head == NULL)
-    {
-        *head = new_node;
-        return (new_node);
-    }
-
-    last_node = *head;
-    while (last_node->next != NULL)
-        last_node = last_node->next;
-
-    last_node->next = new_node;
-    return (new_node);
+	if (*head == NULL)
+		*head = new_node;
+	else
+	{
+		temp_node = *head;
+		while (temp_node->next)
+			temp_node = temp_node->next;
+		temp_node->next = new_node;
+	}
+	return (new_node);
 }
