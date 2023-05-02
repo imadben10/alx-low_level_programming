f23fc42d21e552088dce9d3911b4e7138edb3796
imadben10 <off.imad@gmail.com>
2023-05-02 08:56:09 -0700

    updating

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,15 +1,19 @@
 section .data
-    hello db "Hello, Holberton",10,0
+    hello db "Hello, Holberton",10
 
 section .text
     global main
 
     extern printf
 
+    print_hello_msg:
+        push hello
+        call printf
+        add rsp, 8
+        ret
+
 main:
-    push hello
-    call printf
-    add rsp, 8
+    call print_hello_msg
 
     mov eax, 0
     ret
