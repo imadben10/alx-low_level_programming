48681e1c399ba1a42a48273dab634512840aa5e9
imadben10 <off.imad@gmail.com>
2023-05-01 17:27:28 -0700

    last one

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,49 +1,21 @@
 #include "main.h"
+#include <stdlib.h>
 #include <stdio.h>
-void *malloc_checked(unsigned int b);
 
 /**
- * main - check the code
+ * malloc_checked - allocates memory using malloc
+ * @b: size of memory to allocate
  *
- * Return: Always 0.
- */
-int main(void)
-{
-    char *c;
-    int *i;
-    float *f;
-    double *d;
-
-    c = malloc_checked(sizeof(char) * 1024);
-    printf("%p\n", (void *)c);
-    i = malloc_checked(sizeof(int) * 402);
-    printf("%p\n", (void *)i);
-    f = malloc_checked(sizeof(float) * 100000000);
-    printf("%p\n", (void *)f);
-    d = malloc_checked(sizeof(double) * 1024 * 1024);
-    printf("%p\n", (void *)d);
-    free(c);
-    free(i);
-    free(f);
-    free(d);
-    return (0);
-}
-
-/**
- * malloc_checked - Allocates memory using malloc
- *
- * @b: Size in bytes to be allocated
- *
- * Return: A pointer to the allocated memory, or if malloc fails,
- * the function causes normal process termination with a status value of 98.
+ * Return: pointer to the allocated memory, or exit with status 98 if malloc fails
  */
 void *malloc_checked(unsigned int b)
 {
-    void *p;
+    void *ptr;
 
-    p = malloc(b);
-    if (p == NULL)
+    ptr = malloc(b);
+    if (ptr == NULL)
+    {
         exit(98);
-
-    return (p);
+    }
+    return (ptr);
 }
