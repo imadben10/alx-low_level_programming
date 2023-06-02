d0402aa97dad24a6d6bf949c4815c0cf90752f71
imadben10 <125483039+imadben10@users.noreply.github.com>
2023-06-02 18:34:14 +0200

    differ program

--- 0x09-static_libraries/0-memset.c
+++
@@ -1,8 +0,0 @@
-#include "main.h"
-char *_memset(char *s, char b, unsigned int n)
-{
-    char *ptr = s;
-    while (n--)
-        *ptr++ = (unsigned char) b;
-    return (s);
-}
--- 0x09-static_libraries/1-memcpy.c
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-char *_memcpy(char *dest, char *src, unsigned int n)
-{
-char *d = dest;
-const char *s = src;
-while (n--)
-*d++ = *s++;
-return (dest);
-}
